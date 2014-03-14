1c71f6ae8c4335b0149012286abc6353b3533e6a
David Sargeant <david@dsargeant.com>
2014-03-14 12:16:00 -0400

    Added URL to project file.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   Clojure only at the moment."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/stch-library/schema"
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[speclj "3.0.2"]]}}
   :plugins [[speclj "3.0.2"]]
